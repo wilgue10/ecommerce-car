6295aec4153d3c1b9d97135625b06df50a9c8475
Sarahi Caza <caza.sarahi.cal@gmail.com>
2024-06-30 22:52:58 -0500

    sc 30-06-2024 Start design header

--- frontend/e-commerce-client/src/app/angular-material-module/angular-material-module.module.ts
+++ frontend/e-commerce-client/src/app/angular-material-module/angular-material-module.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatIconModule
   ],
   exports:[
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatIconModule
   ]
 })
 export class AngularMaterialModuleModule { }
