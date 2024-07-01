00fd1de0980f8372a9300fb55d01a1c9d2bed063
Wilmer Guevara <wilmer.guevara@outlook.es>
2024-06-30 22:58:37 -0500

    wg 30-06-2024 set icons on the projects

--- frontend/e-commerce-client/src/app/core/core.module.ts
+++ frontend/e-commerce-client/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AngularMaterialModuleModule } from '../angular-material-module/angular-material-module.module';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AngularMaterialModuleModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
