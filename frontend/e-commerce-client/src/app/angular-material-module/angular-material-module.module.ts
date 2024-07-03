be7e5aa71ddb0d2191adeb438aa914a1d2c824ea
Sarahi Caza <caza.sarahi.cal@gmail.com>
2024-07-02 22:00:00 -0500

    sc 02-07-2024 create menu responsive and serveral changes in styles of header

--- frontend/e-commerce-client/src/app/angular-material-module/angular-material-module.module.ts
+++ frontend/e-commerce-client/src/app/angular-material-module/angular-material-module.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatIconModule} from '@angular/material/icon';
+import { MatMenuModule} from '@angular/material/menu';
+import { MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatSlideToggleModule,
-    MatIconModule
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   exports:[
     MatSlideToggleModule,
-    MatIconModule
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule
   ]
 })
 export class AngularMaterialModuleModule { }
--- frontend/e-commerce-client/src/app/core/core.module.ts
+++ frontend/e-commerce-client/src/app/core/core.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AngularMaterialModuleModule } from '../angular-material-module/angular-material-module.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/**
+ * CoreModule provides shared services, modules, and components across the entire application.
+ */
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, AngularMaterialModuleModule],
+  imports: [CommonModule, BrowserAnimationsModule, AngularMaterialModuleModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
