f37b8eff86be083cbd3bd584eb232e762f4961ec
Wilmer Guevara <wilmer.guevara@terradotta.com>
2024-07-25 12:42:22 -0500

    wg 25-07-2024 implemented login page

--- frontend/e-commerce-client/src/app/angular-material-module/angular-material-module.module.ts
+++ frontend/e-commerce-client/src/app/angular-material-module/angular-material-module.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatSlideToggleModule,
-    MatIconModule
+    MatIconModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSelectModule,
+    MatOptionModule,
   ],
-  exports:[
+  exports: [
     MatSlideToggleModule,
-    MatIconModule
+    MatIconModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSelectModule,
+    MatOptionModule,
   ]
 })
 export class AngularMaterialModuleModule { }
