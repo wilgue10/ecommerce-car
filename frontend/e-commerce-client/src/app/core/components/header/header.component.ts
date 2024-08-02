0f9a716aa90d77f15932eab789f102cd4baf193c
Wilmer Guevara <wilmer.guevara@outlook.es>
2024-08-01 22:22:36 -0500

    wg 01-08-2024 implemented header with array items

--- frontend/e-commerce-client/src/app/core/components/header/header.component.ts
+++ frontend/e-commerce-client/src/app/core/components/header/header.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  items = [
+    { url: 'Item', label: 'Item 1', icon: 'td-car-icon' },
+    { url: 'Item', label: 'Item 2', icon: 'td-car-icon' },
+    { url: 'Item', label: 'Item 3', icon: 'td-car-icon' },
+    { url: 'Item', label: 'Item 4', icon: 'td-car-icon' },
+    { url: 'Item', label: 'Item 5', icon: 'td-car-icon' },
+  ];
 }
